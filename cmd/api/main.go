c1c3c5789454019e1fa8abb56edd786effbd73bf
Pataparapum <cristophercc67@gmail.com>
2026-08-21 04:23:14 -0400

    repository implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,16 +4,20 @@ import (
 	"log"
 
 	"github.com/pataparapum/social/internal/env"
+	"github.com/pataparapum/social/internal/env/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 
 	app := &application{
 		cfg,
+		store,
 	}
+
 	mux := app.mount()
 
 	log.Fatal(app.run(mux))
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pataparapum/social
 
 go 1.26.5
 
-require github.com/go-chi/chi/v5 v5.3.2 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.3.2 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
